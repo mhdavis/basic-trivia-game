7d02a12dc2bf7e309dcbbde66a6ab87736a31c5b
Michael Davis <mhdavis1993@gmail.com>
2017-06-05 14:51:28 -0400

    condensed js logic for readability and closed game logic central loop; NEEDS DEBUGGING

--- assets/js/script.js
+++ assets/js/script.js
@@ -99,80 +99,115 @@ const hardQuestions = [
 
 
 // player selects difficulty setting
+$(document).ready(function() {
 
-function runGame() {
-  let correctQuestions = 0;
-  let totalQuestions = 0;
-  let questionsAsked = [];
-  let questionArr;
+  runGame();
 
-  $("#start-game-panel").show();
-  $("#question-panel").hide();
-  $("#correct-panel").hide();
-  $("#wrong-panel").hide();
+  function runGame() {
+    let correctQuestions = 0;
+    let totalQuestions = 0;
+    let questionsAsked = [];
+    let questionsArr;
+
+    $("#start-game-panel").show();
+    $("#question-panel").hide();
+    $("#correct-panel").hide();
+    $("#wrong-panel").hide();
+    $("#final-panel").hide();
 
 
-  $(".difficulty-button").on("click", function (e) {
+    $(".difficulty-button").on("click", function (e) {
 
-    if (e.target.id === "easy-button") {
-      questionArr = easyQuestions;
-      $("#start-game-panel").hide();
-      $("#question-panel").show();
-    } else if (e.target.id === "medium-button") {
-      questionArr = mediumQuestions;
-    } else if (e.target.id === "hard-button") {
-      questionArr = hardQuestions;
-    }
-
-    // FIGURE OUT WHERE TO PUT THIS
-    if (totalQuestions === 5) {
-      return //stop game;
-    }
-  });
-
-  let randNum = Math.floor(Math.random()*questionsArr.length);
-  setQuestion(questionArr[randNum]);
-
-  $(".response-button").on("click", function(ev) {
-    // clicks a response button
-    // if the response button
-    if (questionsAsked.length < 5) {
-      if (ev.target.text === questionArr[randNum].answer) {
-        // if the user gets the question right
-        $("#question-panel").hide();
-        $("#correct-panel").show();
-        correctQuestions++;
-        totalQuestions++;
-        questionsAsked.push(questionsArr[randNum].id);
-        randNum = Math.floor(Math.random()*questionsArr.length);
-        setQuestion(questionArr[randNum]);
-      } else {
-        // if the user gets the question wrong
-        $("#question-panel").hide();
-        $("#wrong-panel").show();
-        totalQuestions++;
-        questionsAsked.push(questionsArr[randNum].id);
-        setQuestion(questionArr[randNum]);
+      function prepFirstQuestion(arr) {
+        let randNum = Math.floor(Math.random() * arr.length);
+        setQuestion(arr[randNum]);
+        questionsAsked.push(arr[randNum].id);
+        $("#start-game-panel").hide();
+        $("#question-panel").show();
       }
-    } else if (questionAsked.length === 5) {
-      // after player finishes answering question display correct/wrong panel
-      // then display final statistics
-      // then offer refresh button
-      // reset game.
 
-    }
+      if (e.target.id === "easy-button") {
+        questionsArr = easyQuestions;
+        prepFirstQuestion(questionsArr);
+
+      } else if (e.target.id === "medium-button") {
+        questionsArr = mediumQuestions;
+        prepFirstQuestion(questionsArr);
+
+      } else if (e.target.id === "hard-button") {
+        questionsArr = hardQuestions;
+        prepFirstQuestion(questionsArr);
+
+      }
+    });
+
+    $(".response-button").on("click", function(ev) {
+      // clicks a response button
+      // if the response button
+      if (questionsAsked.length < 5) {
+        if (ev.target.text === questionArr[randNum].answer) {
+          // if the user gets the question right
+          $("#question-panel").hide();
+          $("#correct-panel").show();
+          correctQuestions++;
+          totalQuestions++;
+          questionsAsked.push(questionsArr[randNum].id);
+
+          let running = true;
+          while (running) {
+              randNum = Math.floor(Math.random()*questionsArr.length);
+              if (questionsAsked.indexOf(randNum) === -1) {
+                  setQuestion(questionArr[randNum]);
+                  running = false;
+              }
+          }
+
+        } else {
+          // if the user gets the question wrong
+          $("#question-panel").hide();
+          $("#wrong-panel").show();
+          totalQuestions++;
+          questionsAsked.push(questionsArr[randNum].id);
+
+          let running = true;
+          while (running) {
+              randNum = Math.floor(Math.random()*questionsArr.length);
+              if (questionsAsked.indexOf(randNum) === -1) {
+                  setQuestion(questionArr[randNum]);
+                  running = false;
+              }
+          }
+
+        }
+      } else if (questionAsked.length === 5) {
+        $("#numerator").html(correctQuestions);
+        $("#denominator").html(totalQuestions);
+        let percentRight = Math.round((correctQuestions/totalQuestions) * 100);
+        $("#percentage-correct").html(percentRight);
+        $(".difficult-button").off();
+        $(".response-button").off();
+        $("#final-panel").show();
+      }
+    });
+  }
+
+  $("#restart-button").on('click', function() {
+      $("#final-panel").hide();
+      runGame();
   });
 
-$(".continue-button").on("click", function(e) {
-    // hides current window and shows question panel with brand new question
-});
+  $(".continue-button").on("click", function(e) {
+      // hides current window and shows question panel with brand new question
+      $("#question-panel").show();
+      $("#correct-panel").hide();
+      $("#wrong-panel").hide();
+  });
 
-function setQuestion(obj) {
-  $("#question-text").html(obj.question);
+  function setQuestion(obj) {
+    $("#question-text").html(obj.question);
 
-  for (i=0; i < obj.responses.length; i++) {
-      $("#" + string(i)).html(obj.responses[i]);
+    for (i=0; i < obj.responses.length; i++) {
+        $("#" + i).html(obj.responses[i]);
+    }
   }
-}
-
-}
+});
